b3412a4c782fae723eba2c34c9aa089b7b7a7db0
slopster <slopster67@users.noreply.github.com>
2026-03-19 14:34:20 +0000

    center bsod and bundle fixedsys font

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/fonts": "fonts" });
 
   return {
     dir: {
